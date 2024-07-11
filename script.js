7d2e9c8c5fd67e5b7fea9e3f9a1277955ce3c66e
softenrj <149652817+softenrj@users.noreply.github.com>
2024-07-11 22:22:16 +0530

    Update script.js
    fix problems

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
 var counter = 0;
 
-document.querySelector(".add-button").addEventListener("click",addList());
-function addList(){
+document.querySelector(".add-button").addEventListener("click", addList);
+function addList() {
     var listItem = document.querySelector("input").value;
+
     if (listItem.trim() !== "") {
         counter++;
         
         document.querySelector(".todo-lists").innerHTML += `
             <div class="ls${counter} list">
-               <p>${listItem}</p>
+                <p>${listItem}</p>
                 <button class="remove">X</button>
             </div>
         `;
-
         document.querySelector("input").value = "";
-        updateRemoveListeners();
+    } else {
+        alert("Please enter a valid to-do item.");
     }
+
     document.querySelectorAll(".remove").forEach(button => {
         button.addEventListener("click", function() {
             this.parentElement.remove();
             counter--;
         });
     });
-}
+};
 
 document.querySelector("input").addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
         addList();
     }
 });
+document.querySelector(".add-button").addEventListener("click", addList);
