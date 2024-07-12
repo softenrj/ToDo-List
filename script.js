af4d5a8a245b375971159eab317a640ec0d2fed8
softenrj <149652817+softenrj@users.noreply.github.com>
2024-07-12 18:28:39 +0530

    Update script.js
    add more functionality

--- script.js
+++ script.js
@@ -1,34 +1,92 @@
-var counter = 0;
+var time=document.querySelector(".main-heading h2");
 
-document.querySelector(".add-button").addEventListener("click", addList);
-function addList() {
-    var listItem = document.querySelector("input").value;
+function Time() {
+    var dateString = new Date();
+    time.innerHTML=dateString.getDay()+dateString.getMonth()+dateString.getFullYear();
+  }
+  
+  var inputField = document.querySelector(".add-list input");
+  var inputVal = inputField.value;
 
-    if (listItem.trim() !== "") {
-        counter++;
+document.querySelector(".add-list input").addEventListener("click",function len(){
+    var l=document.querySelector(".add-list .length").innerHTML=inputVal.length;
+    inputVal=document.querySelector(".add-list input").value;
+    if(l>=10){
+        document.querySelector(".add-list .emoji1").innerHTML="😅";
+
+        if(l>=16){
+            document.querySelector(".add-list .emoji1").innerHTML="🥵";
+            
+        }if(l>=20){
+                document.querySelector(".add-list .emoji1").innerHTML="🤯";
+            }
         
-        document.querySelector(".todo-lists").innerHTML += `
-            <div class="ls${counter} list">
-                <p>${listItem}</p>
-                <button class="remove">X</button>
-            </div>
-        `;
-        document.querySelector("input").value = "";
-    } else {
-        alert("Please enter a valid to-do item.");
     }
+    else{
+        document.querySelector(".add-list .emoji1").innerHTML="😚";
+    }
+    setTimeout(len,100);
+})
+var ran=Math.floor(Math.random()*3)+1;
 
-    document.querySelectorAll(".remove").forEach(button => {
-        button.addEventListener("click", function() {
-            this.parentElement.remove();
-            counter--;
-        });
+document.querySelector(".add-list i").addEventListener("click",addFun);
+function addFun(){
+    if(inputVal.trim()==="") return;
+
+    var listContainer=document.querySelector(".lists");
+    var newitem=document.createElement("div");
+    newitem.className=`list-item list`;
+    if(ran===1){
+        newitem.innerHTML=`
+        <span class="circle">🔴</span>
+        <p>${inputVal}</p>
+        <i class="fa-solid fa-trash"></i>
+    `;
+    ran=Math.floor(Math.random()*3)+1;
+    }
+    else if(ran===2){
+        newitem.innerHTML=`
+        <span class="circle">🟢</span>
+        <p>${inputVal}</p>
+        <i class="fa-solid fa-trash"></i>
+    `;
+    ran=Math.floor(Math.random()*3)+1;
+    }
+    else{
+        newitem.innerHTML=`
+        <span class="circle">🟠</span>
+        <p>${inputVal}</p>
+        <i class="fa-solid fa-trash"></i>
+    `;
+    ran=Math.floor(Math.random()*3)+1;
+    }
+    listContainer.appendChild(newitem);
+    newitem.querySelector(".fa-trash").addEventListener("click", function() {
+        newitem.remove();
     });
-};
+    newitem.querySelector("span").addEventListener("click", function () {
+        var p = newitem.querySelector("p");
+        if (p.style.textDecoration === "line-through") {
+            p.style.textDecoration = "none";
+            p.style.color = "initial"; // Reset to default text color
+        } else {
+            p.style.textDecoration = "line-through";
+            p.style.color = "rgb(106, 104, 104)";
+        }
+    });
+    inputField.value="";
+        
+}
 
-document.querySelector("input").addEventListener("keydown", (event) => {
+function edit(item, inputVal) {
+    var newInputVal = prompt("Edit your task:", inputVal);
+    if (newInputVal !== null && newInputVal.trim() !== "") {
+        item.querySelector("p").innerHTML = newInputVal;
+    }
+}
+
+document.querySelector(".add-list input").addEventListener("keydown", function(event) {
     if (event.key === "Enter") {
-        addList();
+        addFun();
     }
 });
-document.querySelector(".add-button").addEventListener("click", addList);
